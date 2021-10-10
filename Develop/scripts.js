881949d94748d10af71be6d11ef96fe0afd86e38
TrangTheDev <trangh1@hotmail.com>
2021-10-10 11:16:25 +1100

    Commit JS moment code

---
+++ Develop/scripts.js
@@ -0,0 +1,35 @@
+var today =[],
+today = moment().format('MMMM Do YYYY');
+var currentDay = document.getElementById ("#currentDay");
+currentDay.setAttribute("#currentDay",today);
+console.log(today);
+
+
+var timeSlots = {
+time: "12:00"};/*, 
+"1:00", 
+"2:00", 
+"3:00", 
+"4:00",
+"5:00",
+"6:00",
+"7:00",
+"8:00",
+"9:00",
+"10:00",
+"11:00",
+"12:00",
+"13:00",
+"14:00",
+"15:00",
+"16:00",
+"17:00",
+"18:00",
+"19:00",
+"20:00",
+"21:00"};*/
+
+var addSlots = document.getElementById (".col");
+addSlots.setAttribute(".col",[timeSlots]);
+
+
