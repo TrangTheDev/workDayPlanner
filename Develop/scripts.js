a2ddb758bade940f718e42988500af5ff518699b
TrangTheDev <trangh1@hotmail.com>
2021-10-17 10:57:16 +1100

    commit event listener

--- Develop/scripts.js
+++ Develop/scripts.js
@@ -13,13 +13,7 @@ clockEl.textContent = moment().format('DD-MMM-YYYY HH:mm:ss')
 
 
 let times = [
-"24:00",
-"1:00", 
-"2:00", 
-"3:00", 
-"4:00",
-"5:00",
-"6:00",
+"9:00",
 "10:00",
 "11:00",
 "12:00",
@@ -27,23 +21,17 @@ let times = [
 "14:00",
 "15:00",
 "16:00",
-"17:00",
-"18:00",
-"19:00",
-"20:00",
-"21:00",
-"22:00",
-"23:00"];
+"17:00"];
 
 
 function createTimeblock (time) {
-  //a timeblock contian 3 col
+  //a timeblock contain 3 col
   const row = document.createElement('div');
   row.setAttribute('class', 'row');
   
   //col 1 is the time
   const colTime = document.createElement('div');
-  colTime.setAttribute('class', 'col-2 col-time');
+  colTime.setAttribute('class', 'col-2 col-time .time-block');
   colTime.textContent = time;
   row.appendChild(colTime);
 
@@ -65,14 +53,10 @@ function createTimeblock (time) {
   colBtn.appendChild(button);
   row.appendChild(colBtn);
 
-  //what would happen if user click on the button
-  button.addEventListener('click',function(event){
-    event.preventDefault();
-    //we want to save the text in the current time block
-    const button = event.target;
+  //create a save function
+  function saveInput(){
     const textarea = button.parentNode.previousSibling.children[0].value;
     console.log(button.parentNode.previousSibling.children[0].value);
-
 
     const userInput = textArea.value;
     console.log(userInput)
@@ -80,14 +64,25 @@ function createTimeblock (time) {
   
    //save it to local storage
   localStorage.setItem(time,userInput);
+  }
 
-  })
-  
-  //this text should show next time when the user revisit the page
-
+  //what would happen if user click on the button
+  button.addEventListener('click',function(event){
+    event.preventDefault();
+    //we want to save the text in the current time block
+    const button = event.target;
+    saveInput();
+  });
   //what would happen if the user hit enter in the  text area
-  //do the same thing with the button 
-    
+    //do the same thing with the button 
+  textArea.addEventListener('keyup',function(e){
+    e.preventDefault();
+    if (e.key === 13) {
+      saveInput();
+      console.log("this is keyup");
+    }
+  });
+
   return row;
 }
 
@@ -97,41 +92,36 @@ function createTimeblock (time) {
 for (let i = 0; i < times.length; i++) {
   const time = times[i];
   const timeblock = createTimeblock(time);
-  timeblocksContainerEl.appendChild(timeblock)
-}
-
-
-
-
-//when i am presented with timeblocks each time block is color coded to indicate 
-//If time block is passed we want to give background color CSS class
-
-//if the timeblock is current we want to give background color CSS class
-
-//If the timeblock is future we want to give background color CSS class
-
-
-// Function which changes the colors on past, present and future classes
-
-
-function timeColorBlock() {
-  for (let i = 0; i < times.length; i++) {
-    const addColor = timeColorBlock[i]
-    if (i < currentHour) {
-      let colorBlock = "past";
-    }
-    else if (i === currentHour) {
-      let colorBlock = "present";
-    }
-    else if (i > currentHour) {
-      let colorBlock = "future";
-    }
-    
+  timeblocksContainerEl.appendChild(timeblock);
+  //when i am presented with timeblocks each time block is color coded to indicate 
+  //If time block is passed we want to give background color CSS class
+  if (i < currentHour) {
+    console.log("this is the past");
+    timeblocksContainerEl.setAttribute('class', 'past');
   }
-}
+  //if the timeblock is current we want to give background color CSS class
+  else if (i === currentHour) {
+    console.log("this is the present");
+    timeblocksContainerEl.setAttribute('class', 'present');
+  }
+  //If the timeblock is future we want to give background color CSS class
+  else if (i > currentHour) {
+    console.log("this is the future");
+    timeblocksContainerEl.setAttribute('class', 'future');
+  }
+};
 
-function addColor() {
 
-}
+  
+  //input text should show next time when the user revisit the page
+  let saveItem = localStorage.getItem(time,userInput);
+  
+
+
+
+
+
+
+
 
 
